6a4e7fe7fa38c5cc432b90b07a245cf57e2ca11c
mashaBurdo <masha.burdo@gmail.com>
2025-02-15 17:13:33 +0300

    task 4 1-14

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -200,7 +200,7 @@ function getTail(arr, n) {
  *    +'30,31,32,33,34'
  */
 function toCsvText(arr) {
-   throw new Error('Not implemented');
+   return arr.map((el) => el.join(',')).join('\n');
 }
 
 /**
@@ -215,7 +215,7 @@ function toCsvText(arr) {
  *   [ 10, 100, -1 ]      => [ 100, 10000, 1 ]
  */
 function toArrayOfSquares(arr) {
-   throw new Error('Not implemented');
+   return arr.map((el) => el*el)
 }
 
 
